16d01094691bea9f89a7f25acd76fbfb5c65b8ff
Boumediene Salim <salimboumediene@gmail.com>
2018-09-19 13:59:18 +0200

    betterfuckingwebsite

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,27 @@
-var monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
+if(confirm("Voulez-vous visiter le site xxxvidsxxx?")){
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+ }else 
+    {alert("Vous pouvez activer le mode psychédélique!");
+    }
+
+const listPara = document.getElementsByTagName("p");
+//list of all paragraphs
+const listHeading = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+//list of all headings
+
+function changeBackground() 
+    {
+        document.body.style.background = 'magenta';
+
+        for (var i = 0; i < listPara.length; i++)
+        {
+            listPara[i].style.color = "blue";
+            listPara[i].style.fontFamily = "papyrus";
+        }
+        for (var i = 0; i < listHeading.length; i++)
+        {
+            listHeading[i].style.color = "red";
+            listHeading[i].style.fontFamily = "I hate Comic Sans";
+        }
+    }
+            
